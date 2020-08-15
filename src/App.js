9b842b91a698e132ef58b0064c2801aad6f14a52
mugishamatt <mugisham410@gmail.com>
2020-08-14 22:14:41 -0500

    commited to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 1. Create a const variable named fav_film with an object that has two key values, film and director, include the values.
+
+
+const fav_film={
+  films:['avatar','prison break'],
+  director:{
+    firstName:'matt',
+    lastName:'rich',
+  }
+};
+
+// 2. Display the first name and last name into the DOM 
+console.log(fav_film.director.firstName)
+console.log(fav_film.director.lastName)
+
+// * Extra credit * 
+// Create a nested object and render it onto the DOM 
+
+class App extends Component {
+
+  render() {
+  const favorite=fav_film.films[1]
+  console.log(favorite)
+    return (
+      <div className="App">
+      <h1> What is your favorite film that you seen this year? </h1>
+      <h1>my favorite film is {favorite}</h1>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
+
